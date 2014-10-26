5af60dd9f7e6f2cf33f58fd21bad82c4ac1909f3
Kevin O'Leary <kevinoleary94@gmail.com>
2014-10-25 21:00:31 -0400

    replaced tabs with spaces

--- percent.py
+++ percent.py
@@ -2,13 +2,12 @@
 import sys
 from decimal import Decimal
 if __name__== "__main__":
-	old = Decimal(int(sys.argv[1]))
-	new = Decimal(int(sys.argv[2]))
-	change =  int(100 * (new - old) / old)
-	if change > 0:
-		print change,'% increase'
-	elif change < 0: 
-		print abs(change),'% decrease'
-	elif change == 0:
-		print 'no change'
-	
+  old = Decimal(int(sys.argv[1]))
+  new = Decimal(int(sys.argv[2]))
+  change =  int(100 * (new - old) / old)
+  if change > 0:
+    print change,'% increase'
+  elif change < 0: 
+    print abs(change),'% decrease'
+  elif change == 0:
+    print 'no change'
